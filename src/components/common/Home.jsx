f4feaecf246a15fcd046b20b25fbc0419d646d18
AliHossain09 <alihoossainn@gmail.com>
2025-08-16 23:42:45 +0600

    setup fresh react with bootstrap

---
+++ src/components/common/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <>
+    <header className='shadow'>
+        <div className='bg-dark text-center py-3'>
+            <span className='text-white'>Your fashion Parterner</span>
+
+        </div>
+    </header>
+      
+    </>
+  )
+}
+
+export default Home
